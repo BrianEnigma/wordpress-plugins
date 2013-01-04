27d9a32a6a58ab714fbe5d5171c5c9429c669d64
Brian Enigma <brian@netninja.com>
2013-01-04 11:34:15 -0800

    Modified to only link to year+month. The year+month+day was inaccurate.  I believe there's some difference between GMT and local time between my WordPress install and LiveJournal. I may investigate the details some day, but for now, I'll link only down to the level of accuracy I can reasonably provide.

--- lj-old/lj-old.php
+++ lj-old/lj-old.php
@@ -17,10 +17,10 @@ function ljo_content_filter($content)
 {
     if (get_the_time('U') < 1175990400) // 2007-04-08 as Unix timestamp
     {
-        $url = "http://brianenigma.livejournal.com/" . get_the_time('Y') . "/" . get_the_time('m') . "/" . get_the_time('d');
+        $url = "http://brianenigma.livejournal.com/" . get_the_time('Y') . "/" . get_the_time('m') . "/";
         $boilerplate = "Please note that all blog posts before <i>" . LJO_DATE_TEXT . "</i> " .
             "were automatically imported from LiveJournal.  To see the comments and any LiveJournal-specific " .
-            "extras such as polls and user icons, please go to the source posting at " .
+            "extras such as polls and user icons, please find the source posting at " .
             "<a href=\"" . $url . "\">" . $url . "</a>";
         $content = '<div style="font-size:9pt; margin:1em; padding:0.5em 2em; border:solid black 2px; border-radius:15px; -moz-border-radius:15px; -webkit-border-radius:15px; background-color:#cccccc; color:#000000; font-weight:bold;">' .
             '<a href="' . $url . '">' . 
